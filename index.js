b2ac91a2493c6f9c6150f772c233542010fa3cf7
Fawad <rajafawady@gmail.com>
2025-05-13 06:28:36 +0500

    bug fix

--- index.js
+++ index.js
@@ -72,6 +72,6 @@ app.get("/", (req, res) => {
 
 // Start server
 const PORT = process.env.PORT || 3000;
-server.listen(PORT, '10.7.233.95', () => {
+server.listen(PORT, () => {
   console.log(`🚀 Server running on port ${PORT}`);
 });
