0e94a1dbabcc32c315f11bfa96dd843ca6f3707d
Fawad <rajafawady@gmail.com>
2025-05-14 10:33:30 +0500

    removed firebase admin sdk

--- firebase/firebase.js
+++
@@ -1,8 +0,0 @@
-const admin = require("firebase-admin");
-const serviceAccount = require("./fyp-backend-672d5-firebase-adminsdk-fbsvc-64455292ea.json");
-
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-});
-
-module.exports = admin;
